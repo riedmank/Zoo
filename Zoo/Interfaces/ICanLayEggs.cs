b00c1d88ca40d1e3be5e1ccfebb7ecdb98d84133
Kristofer Riedman <riedmank@hotmail.com>
2018-10-16 11:12:27 -0700

    added and implemented interfaces

---
+++ Zoo/Interfaces/ICanLayEggs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface ICanLayEggs
+    {
+        bool CanLayEggs { get; set; }
+
+        int LayEggs();
+    }
+}
